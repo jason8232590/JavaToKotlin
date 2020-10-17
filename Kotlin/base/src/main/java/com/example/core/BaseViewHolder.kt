938522525c72dd5361e1ce0b1920aad931e84e0f
Adam <734975410@qq.com>
2020-10-17 17:48:08 +0800

    done the homework

---
+++ Kotlin/base/src/main/java/com/example/core/BaseViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.core
+
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+open class BaseViewHolder: RecyclerView.ViewHolder {
+
+    constructor(itemView: View): super(itemView){
+
+    }
+
+    val  viewHashMap: MutableMap<Int, View>  =  HashMap()
+
+    fun <T: View> getView(@IdRes id: Int): T? {
+        var view = viewHashMap[id];
+        if (view == null) {
+            view = itemView.findViewById(id);
+            viewHashMap[id] = view
+        }
+        return view as T;
+    }
+
+    fun  setText(@IdRes id: Int ,  text: String ){
+        (getView<View>(id) as TextView).text = text
+    }
+}
---
+++ Kotlin/lesson/src/main/java/com/example/lesson/LessonPresenter.kt
@@ -0,0 +1,52 @@
+package com.example.lesson
+
+import com.example.core.http.EntityCallback
+import com.example.core.http.HttpClient
+import com.example.core.utils.toast
+import com.example.lesson.entity.Lesson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+
+class LessonPresenter {
+
+    var activity: LessonActivity? = null
+
+    companion object{
+        //之前通过java 中的final 修饰为一个编译期常量，编译时已经确定好并且不能被修改
+        //而kotlin中则要使用const来变为一个编译其常量
+        private const val LESSON_PATH: String = "lessons"
+    }
+    constructor(activity: LessonActivity){
+        this.activity = activity
+    }
+
+
+    var lessons: ArrayList<Lesson>  =  arrayListOf()
+
+    private val type: Type = object : TypeToken<List<Lesson>>(){}.type
+
+    fun fetchData() {
+        HttpClient.INSTANCE.get(LESSON_PATH, type, object: EntityCallback<List<Lesson>>{
+
+            override fun onSuccess(entity: List<Lesson>) {
+                this@LessonPresenter.lessons = lessons;
+                activity!!.runOnUiThread{activity!!.showResult(lessons)}
+            }
+
+            override fun onFailure(message: String) {
+                activity!!.runOnUiThread{toast(message);}
+
+            }
+        });
+    }
+
+    fun showPlayback() {
+        var playbackLessons: MutableList<Lesson>  = arrayListOf() ;
+        for ( lesson in lessons) {
+            if (lesson.state == Lesson.State.PLAYBACK) {
+                playbackLessons.add(lesson);
+            }
+        }
+        activity!!.showResult(playbackLessons);
+    }
+}
